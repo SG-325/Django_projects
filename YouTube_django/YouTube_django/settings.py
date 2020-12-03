622b831b0969049817e39f7057304906fc3feb0c
Sona Gevorgyan <gevorgyans325@gmail.com>
2020-12-03 18:05:56 +0400

    YouTube

--- YouTube_django/YouTube_django/settings.py
+++ YouTube_django/YouTube_django/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'YouTube_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+
+YOUTUBE_DATA_API_KEY = 'AIzaSyDz2PvWGb51JDLSdXW5qwTKsdn0rarKqDE'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
